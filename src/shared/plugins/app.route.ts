e7356f299b105e08ef5b1b70213b3caed5720211
zed <zed@zemail.com>
2026-01-19 16:09:11 +0530

    chore: remove unused declarations

--- src/shared/plugins/app.route.ts
+++ src/shared/plugins/app.route.ts
@@ -1,10 +1,8 @@
-import fastify, { FastifyInstance, FastifyPluginOptions } from "fastify";
+import { FastifyInstance, FastifyPluginOptions } from "fastify";
 
-const router = async (app: FastifyInstance, options: FastifyPluginOptions) => {
-  app.get("/", async (req, res) => {
-
-    res.send("ok");
-  });
-};
+const router = async (
+  app: FastifyInstance,
+  options: FastifyPluginOptions,
+) => {};
 
 export const AppRouter = router;
