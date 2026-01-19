11178933ec30279ccfc0b2cacebb97a4fe0fc5a3
zed <zed@zemail.com>
2026-01-19 17:33:18 +0530

    chore: routes as map for dynamic entry

--- src/shared/plugins/app.route.ts
+++ src/shared/plugins/app.route.ts
@@ -1,8 +1,18 @@
 import { FastifyInstance, FastifyPluginOptions } from "fastify";
 
-const router = async (
-  app: FastifyInstance,
-  options: FastifyPluginOptions,
-) => {};
+// { prefix : router }
+const routes: Record<
+  string,
+  (
+    router: FastifyInstance,
+    options: FastifyPluginOptions,
+  ) => void | Promise<void>
+> = {};
+
+const router = async (app: FastifyInstance, options: FastifyPluginOptions) => {
+  for (const [prefix, route] of Object.entries(routes)) {
+    app.register(route, { prefix });
+  }
+};
 
 export const AppRouter = router;
